12c88cd5259c04699f3dfd1cb4bf8dfffd883335
mihai <mihai.tuhari@gmail.com>
2024-03-27 12:38:17 +0200

    feat: etapa 3 - SASS & navigation

--- resurse/js/scripts.js
+++ resurse/js/scripts.js
@@ -1,10 +1,9 @@
+/**
+ * Grouped buttons.
+ *
+ * When one is clicked, we activate it and deactivate the others.
+ */
 document.addEventListener('DOMContentLoaded', function() {
-
-  /**
-   * Grouped buttons.
-   *
-   * When one is clicked, we activate it and deactivate the others.
-   */
   let buttons = document.querySelectorAll('.btn-group .btn');
 
   buttons.forEach(function(button) {
@@ -14,6 +13,39 @@ document.addEventListener('DOMContentLoaded', function() {
       });
 
       event.target.classList.add('active');
+    });
+  });
+});
+
+/**
+ * Website navigation for mobile.
+ */
+document.addEventListener('DOMContentLoaded', function() {
+  let burgerMenu = document.querySelector('.burger-menu');
+  let mainMenu = document.querySelector('nav ul');
+
+  burgerMenu.addEventListener('click', function() {
+    mainMenu.classList.toggle('open');
+
+    setTimeout(function() {
+      mainMenu.querySelectorAll('.nav-text').forEach(function(item) {
+        item.classList.toggle('animate');
+      })
+    }, 200);
+  });
+
+  let submenuItems = document.querySelectorAll('nav ul li');
+
+  submenuItems.forEach(function(item) {
+    item.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      let childMenu = item.querySelector('ul');
+
+      if (childMenu) {
+        item.classList.toggle('active');
+        item.blur();
+      }
     });
   });
 });
